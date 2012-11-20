0d97e0cd67a1483ec8fac2687a43a6e670ab0459
Florent Thévenet <florent.thevenet@laposte.net>
2012-11-21 00:32:38 +0100

    Possibilité de faire des sous pages

--- index.php
+++ index.php
@@ -9,9 +9,18 @@ if (isset($_GET['page'])) {
   $page = 'index';
 }
 
-$fichier = "pages/$page/$page.php";
+if (isset($_GET['subpage'])) {
+  $sub = $_GET['subpage'];
+} else {
+  $sub = $page;
+}
+
+$fichier = "pages/$page/$sub.php";
+$fichier_alt = "pages/$page/$page.php";
 if (is_file($fichier)) {
   require($fichier);
+} else if (is_file($fichier_alt)) {
+  require($fichier_alt);
 } else {
   header("HTTP/1.0 404 Not Found");
   require("includes/404.php");
---
+++ pages/photos/2011.php
@@ -0,0 +1,9 @@
+<?php
+fds_entete("Photos 2011");
+?>
+
+Yooooo !
+
+<?php
+fds_basdepage();
+?>
