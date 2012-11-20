4eb251997e90e78f8a58bbd04e0e0e15c91a9bf8
Florent Thévenet <florent.thevenet@laposte.net>
2012-11-21 00:29:50 +0100

    quelques balises meta

--- includes/entete.php
+++ includes/entete.php
@@ -3,8 +3,11 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title><?php echo $titre; ?></title>
-    <meta name="keywords" lang="fr" content="Fête de la science, UTC" />
-    <meta name="description" lang="fr" content="" />
+    <meta name="keywords" lang="fr" content="Fête de la science, UTC,
+    inscription, village des sciences" />
+    <meta name="description" lang="fr" content="Découvrez la fête de
+    la science à l'UTC; Inscription en ligne pour les écoles, collèges
+    et lycées." />
     <link href="/fetedelascience/style/bootstrap.min.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="/fetedelascience/images/favicon.png"/>
   </head>
