b13c7c4808ddc290456a06e4d50f925b40312fbe
Florent Thévenet <florent.thevenet@laposte.net>
2012-11-21 13:58:18 +0100

    Jquery

--- includes/basdepage.php
+++ includes/basdepage.php
@@ -1,4 +1,5 @@
 </div>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script type="text/javascript" src="/fetedelascience/js/bootstrap.min.js"></script>
 </body>
 </hml>
