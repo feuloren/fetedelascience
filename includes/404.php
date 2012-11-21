84359eb4440fd3a294fe5427eaf97c5c6f8484ea
Florent Thévenet <florent.thevenet@laposte.net>
2012-11-21 13:58:10 +0100

    entete dans 404.php

--- includes/404.php
+++ includes/404.php
@@ -1,7 +1,21 @@
-<!DOCTYPE html>
-<html>
-<h1>Mama mia !</h1>
-The page you a-requested does not exist !<br/>
-<img src="images/mario-sad.png" alt="Mario is a-sad" />
+<?php
+fds_entete("Page introuvable");
+?>
 
-</html>
+<style>
+.fds-404 {
+text-align: center;
+}
+</style>
+
+<div class="row-fluid">
+  <div class="span12 fds-404">
+    <h1>Mama mia !</h1>
+    The page you a-requested does not exist !<br/>
+    <img src="images/mario-sad.png" alt="Mario is a-sad" />
+  </div>
+</div>
+
+<?php
+fds_basdepage();
+?>
