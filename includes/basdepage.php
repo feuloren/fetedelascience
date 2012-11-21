2f7f9973dbbe0c3612b5ad571125741e188cc663
Florent Thévenet <florent.thevenet@laposte.net>
2012-11-21 18:06:28 +0100

    correction </hml> au lieu de </html>

--- includes/basdepage.php
+++ includes/basdepage.php
@@ -1,5 +1,5 @@
-</div>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<script type="text/javascript" src="/fetedelascience/js/bootstrap.min.js"></script>
-</body>
-</hml>
+      </div>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script type="text/javascript" src="/fetedelascience/js/bootstrap.min.js"></script>
+  </body>
+</html>
