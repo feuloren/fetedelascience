cd7a2bdc10bfc197114a108e04fdfaa9c177dd80
Florent Thévenet <florent.thevenet@laposte.net>
2012-11-21 23:37:28 +0100

    Comité

---
+++ pages/comite/comite.php
@@ -0,0 +1,59 @@
+<?php
+fds_entete('Comité Fête de la science 2012');
+?>
+
+<div class="row-fluid">
+  <div class="span12 well well-large">
+    <p>
+      La Fête de la Science de l'UTC nécessite une activité tout au long de l'année de l'équipe coordinatrice et du Comité de Pilotage,
+      constitué de correspondants de différents départements et services de l'UTC et de partenaires extérieurs.
+    </p>
+    <h3>Correspondants :</h3>
+    <ul class="unstyled">
+      <li>Jean-Luc DUVAL  (UTC Génie Biologique)</li>
+      <li>Stéphane OCTAVE (UTC Génie Biologique - Laboratoire GEC)</li>
+      <li>Audrey DRELICH (UTC Génie des Procédés Industriels)</li>
+      <li>Fabienne FAMCHON (ESCOM)</li>
+      <li>Benjamin LUSSIER (UTC Génie Informatique)</li>
+      <li>Eduard ANTALUCA (UTC Génie des Systèmes Urbains) </li>
+      <li>Isabel GUGLIELMONE (UTC Technologies et Sciences de l'Homme)</li>
+      <li>Emmanuel Corbasson (UTC Génie Mécanique)</li>
+      <li>Sylvie LEFEBVRE (UTC Génie des Systèmes Urbains)</li>
+    </ul>
+    <h3>Avec le soutien de :</h3>
+    <ul class="unstyled">
+	    <li>Zohra CHERFI (UTC Direction des Enseignements et de la Pédagogie)</li>
+	    <li>Sébastien BRIGLIADORI (UTC Patrimoine Immobilier)</li>
+	    <li>Maïté ROUSSEL (UTC Service Accueil)</li>
+	    <li>Thierry THOMASSET (UTC Conseil scientifique)</li>
+      <li>Peggy BAILLY (<a href="http://www.echopresse.fr/">echopresse.fr</a>)</li>
+      <li>François GRAUX</li>
+	    <li>Sylviane HÉAU</li>
+    </ul>
+    <h3>Coordination :</h3>
+    <ul class="unstyled">
+	    <li>Élisabeth BRUNIER (UTC Service Culture Scientifique et Technologique - responsable de la manifestation)</li>
+	    <li>Aurélie SALIN (UTC Service Culture Scientifique et Technologique)</li>
+    </ul>
+    <h3>Equipe d'étudiants-assistants :</h3>
+    <ul class="unstyled">
+      <li>Cyrine MADHI</li>
+      <li>Susie BARBEAU</li>
+      <li>Clémence BACQUET</li>
+      <li>Guillaume De FOUCAULT</li>
+      <li>Alban MORZYGLOD</li>
+      <li>Mathilde PADILLA</li>
+      <li>Julia LEGRAND</li>
+      <li>Jason-Marlow CHERADAME</li>
+    </ul>
+    <p>
+      Ces étudiants sont chargés de la communication, de la scénographie
+      et des relations avec les porteurs de projet. Chacun encadrera au
+      moment de la manifestation une équipe d'étudiants-guides.
+    </p>
+  </div>
+</div>
+
+<?php
+fds_basdepage();
+?>
