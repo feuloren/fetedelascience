d554bf396fb439c0bff7cca49a7935561b2c4b8c
Matthieu <matgrillere@gmail.com>
2012-11-22 16:05:29 +0100

    bas de page a jour

--- includes/basdepage.php
+++ includes/basdepage.php
@@ -1,3 +1,43 @@
+<div class="row-fluid fds-footer">
+          <div class="span2">
+            <span class="label label-info">
+            Nos partenaires :
+            </span>
+          </div>
+          <div class="span2 offset8">
+            <a href="#">Retour vers haut de page <i class="icon-arrow-up"></i></a>
+          </div>
+        <div class="row-fluid">
+          <div class="span2">
+            <img src="/fetedelascience/images/bas/logo_utc.png" alt="logo_utc">
+          </div>
+          <div class="span1">
+            <img src="/fetedelascience/images/bas/picardie.jpg" alt="logo_picardie">
+          </div>
+          <div class="span1">
+            <img src="/fetedelascience/images/bas/escom.png" alt="logo_escom">
+          </div>
+          <div class="span1">
+            <img src="/fetedelascience/images/bas/logo_amiens.png" alt="logo_amiens">
+          </div>
+          <div class="span1">
+            <img src="/fetedelascience/images/bas/logo_arc_compiegne.png" alt="logo_arc_compiegne">
+          </div>
+          <div class="span1">
+            <img src="/fetedelascience/images/bas/logo_compiegne.png" alt="logo_compiegne">
+          </div>
+          <div class="span2">
+            <img src="/fetedelascience/images/bas/ombelliscience.png" alt="logo_ombelliscience">
+          </div>
+          <div class="span1">
+            <img src="/fetedelascience/images/bas/jeanlegendre.jpg" alt="logo_jeanlegendre">
+          </div>
+          <div class="span1">
+            <img src="/fetedelascience/images/bas/recherche.png" alt="logo_recherche">
+          </div>
+        </div>
+        </br>
+      </div>
       </div>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <script type="text/javascript" src="/fetedelascience/js/bootstrap.min.js"></script>
