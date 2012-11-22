9b6d7c215246cdfe38427c94a193c3924729a9c4
Matthieu <matgrillere@gmail.com>
2012-11-22 16:03:21 +0100

    bas de page

--- includes/basdepage.php
+++ includes/basdepage.php
@@ -1,6 +1,5 @@
       </div>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <script type="text/javascript" src="/fetedelascience/js/bootstrap.min.js"></script>
-    <?php echo $additional_scripts; ?>
   </body>
 </html>
