41035b2e37e9f9c46918595c9de943063b3cb8fa
Florent Thévenet <florent.thevenet@laposte.net>
2012-11-23 19:00:31 +0100

    Modifs basdepage que Mathieu avait virées

--- includes/basdepage.php
+++ includes/basdepage.php
@@ -40,7 +40,8 @@
         </div>
       </div>
       </div>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
     <script type="text/javascript" src="/fetedelascience/js/bootstrap.min.js"></script>
+   <?php echo $additional_scripts; ?>
   </body>
 </html>
