99ad18ec772cbd7f3b4b2765acd04d6f987322eb
Florent Thévenet <florent@fthevenet.fr>
2013-03-03 03:04:47 +0100

    Village : js pour l'ajout d'accompagnateurs et le choix d'établissement

--- pages/inscription/village.php
+++ pages/inscription/village.php
@@ -1,11 +1,24 @@
 <?php
 fds_entete("Inscription au Village");
+
+$etablissements = db_query("SELECT id, nom, ville FROM etablissements13");
 ?>
 <div class="row-fluid">
   <div class="span12 well well-large">
 		<form method=POST action= inscription/villageprocess >
-				<table>
-					<tr><td>Etablissement:</td>
+                <h2>Informations sur l'établissement</h2>
+                    Établissement: 
+                    <select name="id_etablissement" id="etablissement">
+                            <option value="-1">- Ajouter un établissement -</option>
+                            <option>- - - - -</option>
+                            <?php
+                                while($data = $etablissements->fetch_assoc()) {
+                                    echo "<option value=\"" . $data["id"] . "\">" . $data["nom"] . " - " . $data["ville"] . "</option>";
+                                }
+                            ?>
+                        </select>
+				<table id="newEtab">
+					<tr><td>Nom de l'établissement:</td>
 					<td><input type=text name=etablissement size=50></td></tr>
 					<tr><td>Adresse (nom de rue):</td>
 					<td><input type=text name=rue size=50></td></tr>
@@ -21,54 +34,15 @@ fds_entete("Inscription au Village");
 					<td><input type=text name=email size=50></td></tr>
 				</table>
 				</br>
-				<p>
+				<h2>
 					Informations sur les accompagnateurs :
-				</p>
-					<div class="row-fluid">
-						<div class="span3">
-							 Nom: <input type=text name=ac1nom size=50>
-						</div>
-						<div class="span3">
-							Prénom: <input type=text name=ac1prenom size=50>
-						</div>
-						<div class="span3">
-							Télephone: <input type=text name=ac1telephone size=50>
-						</div>
-						<div class="span3">
-							E-mail: <input type=text name=ac1mail size=50>
-						</div>
-					</div>
-					<div class="row-fluid">
-						<div class="span3">
-							 Nom: <input type=text name=ac1nom size=50>
-						</div>
-						<div class="span3">
-							Prénom: <input type=text name=ac1prenom size=50>
-						</div>
-						<div class="span3">
-							Télephone: <input type=text name=ac1telephone size=50>
-						</div>
-						<div class="span3">
-							E-mail: <input type=text name=ac1mail size=50>
-						</div>
-					</div>
-					<div class="row-fluid">
-						<div class="span3">
-							 Nom: <input type=text name=ac1nom size=50>
-						</div>
-						<div class="span3">
-							Prénom: <input type=text name=ac1prenom size=50>
-						</div>
-						<div class="span3">
-							Télephone: <input type=text name=ac1telephone size=50>
-						</div>
-						<div class="span3">
-							E-mail: <input type=text name=ac1mail size=50>
-						</div>
-					</div>
-					<p>
+				</h2>
+					<div class="row-fluid" id="listeIntervenants">
+                    </div>
+                    <a href="#listeIntervenants" id="btnAddIntervenant" class="btn btn-primary">Ajouter un Intervenant</a>
+					<h2>
 						Information complémentaire :
-					</p>
+					</h2>
 					<table>
 						<tr>
 							<td>Niveau: </td>
@@ -96,12 +70,46 @@ fds_entete("Inscription au Village");
 							Heure départ: <input type=text name=nombre size=50>
 						</div>
 					</div>
-				<input type=submit value=Envoyer> -
-				<input type=reset value=Annuler>
-			</form>
-  </div>
+                <br/><br/>
+				<input type=submit value=Envoyer class="btn btn-success btn-large"> 
+				<input type=reset value=Annuler class="btn btn-danger btn-large">
+            </form>
+    </div>
 </div>
 
 <?php
-fds_basdepage();
+$script = <<<'SCRIPT'
+    var nb_intervenants = 0;
+    var add_intervenant = function() {
+        var txt = '<div class="row-fluid">'+
+                  '<div class="span3">'+
+                  'Nom: <input type=text name=ac'+nb_intervenants+'nom size=50>'+
+                  '</div>'+
+                  '<div class="span3">'+
+                  'Prénom: <input type=text name=ac'+nb_intervenants+'prenom size=50>'+
+                  '</div>'+
+                  '<div class="span3">'+
+                  'Télephone: <input type=text name=ac'+nb_intervenants+'telephone size=50>'+
+                  '</div>'+
+                  '<div class="span3">'+
+                  'E-mail: <input type=text name=ac'+nb_intervenants+'mail size=50>'+
+                  '</div>'+
+                  '</div>';
+        $("#listeIntervenants").append(txt);
+        nb_intervenants += 1;
+    }
+    $(function() {
+        $("#btnAddIntervenant").click(add_intervenant);
+        add_intervenant();
+        $("#newEtab").hide();
+        $("#etablissement").change(function() {
+            if ($(this).val() == -1)
+                $("#newEtab").show();
+            else
+                $("#newEtab").hide();
+        });
+    });
+SCRIPT;
+
+fds_basdepage($script);
 ?>
